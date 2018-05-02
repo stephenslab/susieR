d29bb567e6a3d7e220347103c7491cae0023ee5e
stephens999 <stephens999@gmail.com>
2018-05-02 11:37:09 -0500

    update docs for coef and predict methods

--- R/predict.susie.R
+++ R/predict.susie.R
@@ -1,10 +1,17 @@
+#' @title extract regression coefficients from susie fit
 #' @param s a susie fit
 #' @return a p vector of estimated regression coefficients
+#' @method coef susie
 #' @export
 coef.susie = function(s){
   colSums(s$alpha*s$mu)
 }
 
+#' @title predict future observations or extract coefficients from susie fit
+#' @param s a susie fit
+#' @param newx a new value for X at which to do predictions
+#' @param type if this is coefficients, then calls coef.susie
+#' @method predict susie
 #' @export
 predict.susie = function(s,newx = NULL,type=c("response","coefficients")){
   type <- match.arg(type)
