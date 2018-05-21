83c5cd13a27b255daacbb0a25c5102e10bd7ac32
Gao Wang <gaow@uchicago.edu>
2018-05-21 12:37:16 -0500

    Change CI to CS and export lfsr and CI functions

--- R/susie_utils.R
+++ R/susie_utils.R
@@ -1,39 +1,48 @@
-# This computes the average lfsr across SNPs for each l, weighted by the
-# posterior inclusion probability alpha
-lfsr_fromfit = function(res){
+#' @title Local false sign rate (lfsr) for susie confidence sets
+#' @details This computes the average lfsr across SNPs for each l, weighted by the
+#' posterior inclusion probability alpha
+#' @param fitted a susie fit, the output of `susieR::susie()`
+#' @return an l vector of lfsr for confidence sets
+#' @export
+susie_get_lfsr = function(res){
   pos_prob = pnorm(0,mean=t(res$mu),sd=sqrt(res$mu2-res$mu^2))
   neg_prob = 1-pos_prob
   1-rowSums(res$alpha*t(pmax(pos_prob,neg_prob)))
 }
 
-#find how many variables in the 95% CI
+#find how many variables in the 95% CS
 # x is a probability vector
-n_in_CI_x = function(x){
+n_in_CS_x = function(x){
   sum(cumsum(sort(x,decreasing = TRUE))<0.95)+1
 }
 
-# return binary vector indicating if each point is in CI
+# return binary vector indicating if each point is in CS
 # x is a probability vector
-in_CI_x = function(x){
-  n = n_in_CI_x(x)
+in_CS_x = function(x){
+  n = n_in_CS_x(x)
   o = order(x,decreasing=TRUE)
   result = rep(0,length(x))
   result[o[1:n]] = 1
   return(result)
 }
 
-# Return binary matrix indicating which variables are in CI of each
-# of effect
-in_CI = function(res){
+#' @title Variables in susie confidence sets
+#' @details It returns a binary matrix indicating which variables are in CS
+#' of each effect
+#' @param fitted a susie fit, the output of `susieR::susie()`, or simply the posterior
+#' inclusion probability matrix alpha
+#' @return a l by p binary matrix
+#' @export
+susie_in_CS = function(res){
   if (class(res) == "susie")
     res = res$alpha
-  t(apply(res,1,in_CI_x))
+  t(apply(res,1,in_CS_x))
 }
 
-n_in_CI = function(res){
+n_in_CS = function(res){
   if (class(res) == "susie")
     res = res$alpha
-  apply(res,1,n_in_CI_x)
+  apply(res,1,n_in_CS_x)
 }
 
 # computes z score for association between each
@@ -47,9 +56,9 @@ calc_z = function(X,y){
 }
 
 
-# plot p values of data and color in the 95% CIs
+# plot p values of data and color in the 95% CSs
 # for simulated data, specify b = true effects (highlights in red)
-pplot = function(X,y,res,pos=NULL,b=NULL,CImax = 400,...){
+pplot = function(X,y,res,pos=NULL,b=NULL,CSmax = 400,...){
   z = calc_z(X,y)
   zneg = -abs(z)
   logp = log10(pnorm(zneg))
@@ -58,8 +67,8 @@ pplot = function(X,y,res,pos=NULL,b=NULL,CImax = 400,...){
   plot(pos,-logp,col="grey",xlab="",ylab="-log10(p)",...)
   points(pos[b!=0],-logp[b!=0],col=2,pch=16)
   for(i in 1:nrow(res$alpha)){
-    if(n_in_CI(res)[i]<CImax)
-      points(pos[which(in_CI(res)[i,]>0)],-logp[which(in_CI(res)[i,]>0)],col=i+2)
+    if(n_in_CS(res)[i]<CSmax)
+      points(pos[which(in_CS(res)[i,]>0)],-logp[which(in_CS(res)[i,]>0)],col=i+2)
   }
 
 }
