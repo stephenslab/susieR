7898b6be2ca3422691539ebf0ac9648c1687be9d
KaiqianZhang <zhangkai17@grinnell.edu>
2018-08-20 13:29:56 -0500

    add Matrix:: to fix t() and tcrossprod()

--- R/sparse_multiplications.R
+++ R/sparse_multiplications.R
@@ -8,8 +8,8 @@ compute_sparse_Xy = function(X.sparse, y, cm, csd){
     return(X.sparse%*%y)
   }else{
     #scale Xy
-    scaled.X = t(t(X.sparse)/csd)
-    scaled.Xy = tcrossprod(scaled.X, t(y))
+    scaled.X = Matrix::t(Matrix::t(X.sparse)/csd)
+    scaled.Xy = Matrix::tcrossprod(scaled.X, t(y))
     #center Xy
     Xy = scaled.Xy - sum(cm*y/csd) 
     return(as.numeric(Xy))
@@ -25,8 +25,8 @@ compute_sparse_Xty = function(X.sparse, y, cm, csd){
   if(is.matrix(X.sparse)){
     return(t(X.sparse)%*%y)
   }else{
-    Xty = crossprod(X.sparse, y)
-    scaled.Xty = t(t(Xty)/csd)
+    Xty = Matrix::crossprod(X.sparse, y)
+    scaled.Xty = Matrix::t(Matrix::t(Xty)/csd)
     centered.scaled.Xty =scaled.Xty - cm/csd * sum(y)     
     return(as.numeric(centered.scaled.Xty))
   }
