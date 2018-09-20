227576d785d8db0f2106b3734ac452b52cc4407a
Peter Carbonetto <peter.carbonetto@gmail.com>
2018-09-19 21:53:19 -0500

    Removed FIXME comment.

--- R/sparse_multiplication.R
+++ R/sparse_multiplication.R
@@ -8,8 +8,6 @@ compute_Xb = function(X, b){
   if (is.matrix(X)) { #when X is a dense matrix
     return(tcrossprod(X,t(b))) #tcrossprod(A,B) performs A%*%t(B) but faster
   } else {
-    ## FIXME: sparse matrix multiplication advantage will be diluted
-    ## given larger p since using many transpose here.
     cm = attr(X, 'scaled:center')
     csd = attr(X, 'scaled:scale')
     #scale Xb
@@ -17,7 +15,6 @@ compute_Xb = function(X, b){
     #center Xb
     Xb <- scaled.Xb - sum(cm*b/csd)
     return(as.numeric(Xb))
-    #return(tcrossprod(attr(X, 'scaled.X'),t(b)))
  }
 }
 
