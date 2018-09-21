30f32d54f1b110b4d0d6427cbbb8da345139e068
KaiqianZhang <zhangkai17@grinnell.edu>
2018-09-21 10:10:23 -0500

    change safe_colScale test

--- tests/testthat/test_sparse_safe_colScale.R
+++ tests/testthat/test_sparse_safe_colScale.R
@@ -6,15 +6,7 @@ test_that("sparse version safe_colScale",{
   dense.res = safe_colScale(X)
   sparse.res = safe_colScale(X.sparse)
   
-  dense.faceX = dense.res
-  attributes(dense.faceX) = NULL
-  attributes(X) = NULL
-  
-  sparse.faceX = sparse.res
-  attributes(sparse.faceX) = NULL
-  attributes(X.sparse) = NULL
-  
-  expect_equal(sparse.faceX, X.sparse)
+  expect_equal(sparse.res, X.sparse, check.attributes=FALSE)
   expect_equal(attr(dense.res, 'scaled:center'), attr(sparse.res, 'scaled:center'))
   expect_equal(attr(dense.res, 'scaled:scale'), attr(sparse.res, 'scaled:scale'))
   expect_equal(attr(dense.res, 'd'), attr(sparse.res, 'd'))
