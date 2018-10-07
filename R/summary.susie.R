8ee0c7c1d6f1189a184468ae75af36214f73fc26
Gao Wang <gaow@uchicago.edu>
2018-10-07 11:55:19 -0500

    Add summary function for susie fit

---
+++ R/summary.susie.R
@@ -0,0 +1,23 @@
+#' @title summarize susie fit
+#' @param s a susie fit
+#' @return a data.frame of variables and a data.frame of credible sets
+#' @method summary susie
+#' @export
+summary.susie = function(s) {
+  if (is.null(s$sets))
+    stop("Cannot summarize SuSiE object because credible set information is not available")
+  cs = data.frame(matrix(NA, length(s$sets$cs), 5))
+  colnames(cs) = c('cs', 'cs_log10bf', 'cs_avg_r2', 'cs_min_r2', 'variable')
+  variables = data.frame(cbind(1:length(s$pip), s$pip, -1))
+  colnames(variables) = c('variable', 'variable_prob', 'cs')
+  for (i in 1:length(s$sets$cs)) {
+    variables$cs[variables$variable %in% s$sets$cs[[i]]] = s$sets$cs_index[[i]]
+    cs$cs = s$sets$cs_index[[i]]
+    cs$cs_log10bf = s$lbf[cs$cs]
+    cs$cs_avg_r2 = s$sets$purity$mean.abs.corr^2
+    cs$cs_min_r2 = s$sets$purity$min.abs.corr^2
+    cs$variable = paste(s$sets$cs[[i]],collapse=',')
+  }
+  variables = variables[order(variables$variable_prob, decreasing = T),]
+  return(list(vars=variables, cs=cs))
+}
