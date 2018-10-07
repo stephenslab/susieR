e3eb951fc29a208f6fe21cf712d3426461b69a45
Gao Wang <gaow@uchicago.edu>
2018-10-07 14:15:13 -0500

    Run unit test in precommit hook

--- inst/misc/pre-commit.sh
+++ inst/misc/pre-commit.sh
@@ -23,8 +23,14 @@ else
     echo "Version string bumped to revision $REV_ID on $DATE"
     sed -i "s/$SED_REGEX/Version: \1.$REV_ID/" $ROOT_DIR/DESCRIPTION
     sed -i "s/^Date: .*/Date: $DATE/" $ROOT_DIR/DESCRIPTION
-    echo "Updating documentation ..."
-    cd $ROOT_DIR && Rscript -e 'devtools::document()' &> /dev/null && git add man/*.Rd
-    echo "Documentation updated!"
+    if [[ `git rev-parse --abbrev-ref HEAD` -eq "master" ]]; then
+        cd $ROOT_DIR
+        echo "Updating documentation ..."
+        Rscript -e 'devtools::document()' &> /dev/null && git add man/*.Rd
+        echo "Documentation updated!"
+        echo "Running unit tests ..."
+        Rscript -e 'devtools::test()'
+        echo "Unit test completed!"
+    fi
     exit 0
 fi
