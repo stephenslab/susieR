fb08a85f5f1e85fca5512def89896258a1a1720d
Gao Wang <gaow@uchicago.edu>
2018-10-07 18:56:44 -0500

    Set CS to NULL in summary if get_CS is empty

--- R/summary.susie.R
+++ R/summary.susie.R
@@ -6,20 +6,24 @@
 summary.susie = function(s) {
   if (is.null(s$sets))
     stop("Cannot summarize SuSiE object because credible set information is not available")
-  cs = data.frame(matrix(NA, length(s$sets$cs), 5))
-  colnames(cs) = c('cs', 'cs_log10bf', 'cs_avg_r2', 'cs_min_r2', 'variable')
   variables = data.frame(cbind(1:length(s$pip), s$pip, -1))
   colnames(variables) = c('variable', 'variable_prob', 'cs')
-  if (s$null_index > 0) variables = variables[-s$null_index,]
-  for (i in 1:length(s$sets$cs)) {
-    variables$cs[variables$variable %in% s$sets$cs[[i]]] = s$sets$cs_index[[i]]
-    cs$cs[i] = s$sets$cs_index[[i]]
-    cs$cs_log10bf[i] = s$lbf[cs$cs[i]]
-    cs$cs_avg_r2[i] = s$sets$purity$mean.abs.corr[cs$cs[i]]^2
-    cs$cs_min_r2[i] = s$sets$purity$min.abs.corr[cs$cs[i]]^2
-    cs$variable[i] = paste(s$sets$cs[[i]], collapse=',')
-  }
-  variables = variables[order(variables$variable_prob, decreasing = T),]
   rownames(variables) = NULL
+  if (s$null_index > 0) variables = variables[-s$null_index,]
+  if (!is.null(s$sets$cs)) {
+      cs = data.frame(matrix(NA, length(s$sets$cs), 5))
+      colnames(cs) = c('cs', 'cs_log10bf', 'cs_avg_r2', 'cs_min_r2', 'variable')
+      for (i in 1:length(s$sets$cs)) {
+        variables$cs[variables$variable %in% s$sets$cs[[i]]] = s$sets$cs_index[[i]]
+        cs$cs[i] = s$sets$cs_index[[i]]
+        cs$cs_log10bf[i] = s$lbf[cs$cs[i]]
+        cs$cs_avg_r2[i] = s$sets$purity$mean.abs.corr[cs$cs[i]]^2
+        cs$cs_min_r2[i] = s$sets$purity$min.abs.corr[cs$cs[i]]^2
+        cs$variable[i] = paste(s$sets$cs[[i]], collapse=',')
+      }
+      variables = variables[order(variables$variable_prob, decreasing = T),]
+  } else {
+      cs = NULL
+  }
   return(list(vars=variables, cs=cs))
 }
