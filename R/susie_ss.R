061562a68ed52a34dad061ac3121c7e5fff7c888
zouyuxin <yuxin.zou.stat@gmail.com>
2018-10-12 15:07:32 -0500

    Update susie_ss interface

--- R/susie_ss.R
+++ R/susie_ss.R
@@ -85,8 +85,8 @@ susie_ss = function(XtX, Xty, n, var_y = 1, L=10,
 
   if(standardize){
     dXtX = diag(XtX)
-    dXtX[dXtX==0] = 1
     csd = sqrt(dXtX/(n-1))
+    csd[csd == 0] = 1
     XtX = (1/csd) * t((1/csd) * XtX)
     Xty = (1/csd) * Xty
   }else{
@@ -153,22 +153,12 @@ susie_ss = function(XtX, Xty, n, var_y = 1, L=10,
   return(s)
 }
 
-#' @title Apply susie with z scores and correlation matrix R
+#' @title Apply susie with z scores and cov/cor matrix R
 #' @param z a p vector of z scores.
-#' @param R a p by p symmetric and positive semidefinite matrix
-#' @param n sample size
-#' @param var_y the (sample) variance of the y
-#' @param R_type the type of the matrix R, one of 'Cor', 'XtX' or 'Cov'
+#' @param R a p by p symmetric and positive semidefinite matrix. It can be X'X, covariance matrix or correlation matrix.
 #' @param L maximum number of non-zero effects
-#' @param scaled_prior_variance the scaled prior variance (vector of length L, or scalar. In latter case gets repeated L times )
-#' @param estimate_prior_variance indicates whether to estimate prior (currently not recommended as not working as well)
 #' @param prior_weights a p vector of prior probability that each element is non-zero
 #' @param null_weight probability of no effect, for each single effect model
-#' @param standardize logical flag (default=TRUE) for whether to standardize columns of X to unit variance prior to fitting.
-#' Note that `scaled_prior_variance` specifies the prior on the coefficients of X *after* standardization (if performed).
-#' If you do not standardize you may need
-#' to think more carefully about specifying `scaled_prior_variance`.
-#' Whatever the value of standardize, the coefficients (returned from `coef`) are on the input R scale.
 #' @param coverage coverage of confident sets. Default to 0.95 for 95\% confidence interval.
 #' @param min_abs_corr minimum of absolute value of correlation allowed in a confidence set.
 #' @param verbose if true outputs some progress messages
@@ -177,11 +167,9 @@ susie_ss = function(XtX, Xty, n, var_y = 1, L=10,
 #' @return a susie fit
 #'
 #' @export
-susie_z = function(z, R, n, var_y = 1, R_type = c('Cor', 'Cov', 'XtX'),
-                   L=10, scaled_prior_variance=0.2,
-                   estimate_prior_variance = TRUE,
+susie_z = function(z, R,
+                   L=10,
                    prior_weights = NULL, null_weight = NULL,
-                   standardize = TRUE,
                    coverage=0.95, min_abs_corr=0.5,
                    verbose=FALSE, track_fit = FALSE, ...){
   if(nrow(R) != length(z)){
@@ -197,29 +185,29 @@ susie_z = function(z, R, n, var_y = 1, R_type = c('Cor', 'Cov', 'XtX'),
     stop('R is not a positive semidefinite matrix.')
   }
 
-  R_type = match.arg(R_type)
-  if(R_type != 'XtX'){
-    XtX = (n-1)*R
-  }else{
-    XtX = R
-  }
+  X0 = diag(R) == 0
+  # convert any input R to correlation matrix
+  # if R has 0 colums and rows, cov2cor produces NaN and warning
+  R = muffled_cov2cor(R)
+  # change the columns and rows with NaN to 0
+  R[X0, ] = R[,X0] = 0
 
-  susie_ss(XtX = XtX, Xty = z*sqrt(diag(XtX)*var_y),
-           n = n, var_y = var_y, L = L,
-           scaled_prior_variance = scaled_prior_variance,
-           estimate_prior_variance = estimate_prior_variance,
+  susie_ss(XtX = R, Xty = z,
+           n = 2, var_y = 1,
+           L = L,
+           estimate_prior_variance = TRUE,
            prior_weights = prior_weights, null_weight = null_weight,
-           standardize = standardize,
+           standardize = TRUE,
            coverage=coverage, min_abs_corr=min_abs_corr,
            verbose=verbose, track_fit = track_fit, ...)
 }
 
-#' @title Apply susie with bhat, shat and correlation matrix R
+#' @title Apply susie with bhat, shat and cov/cor matrix R
 #' @param bhat a p vector of estimated effects.
 #' @param shat a p vector of corresponding standard errors.
 #' @param R a p by p symmetric and positive semidefinite matrix. It can be X'X, covariance matrix or correlation matrix.
 #' @param n sample size
-#' @param var_y the (sample) variance of the y. If it is unknown, the coefficients (returned from `coef`) are for X and y on the standardized scale.
+#' @param var_y the (sample) variance of the y. If it is unknown, the coefficients (returned from `coef`) are on the standardized X, y scale.
 #' @param L maximum number of non-zero effects
 #' @param scaled_prior_variance the scaled prior variance (vector of length L, or scalar. In latter case gets repeated L times )
 #' @param estimate_prior_variance indicates whether to estimate prior (currently not recommended as not working as well)
@@ -246,8 +234,8 @@ susie_bhat = function(bhat, shat, R, n, var_y = 1,
                       standardize = TRUE,
                       coverage=0.95, min_abs_corr=0.5,
                       verbose=FALSE, track_fit = FALSE, ...){
-  if(length(bhat) != length(shat)){
-    stop('The length of bhat does not agree with length of shat.')
+  if(missing(n)){
+    stop('n must be provided. If there is no information about n, use susie_z instead.')
   }
   if(nrow(R) != length(bhat)){
     stop('The dimension of R does not agree with length of bhat.')
@@ -255,14 +243,28 @@ susie_bhat = function(bhat, shat, R, n, var_y = 1,
   if(!isSymmetric(R)){
     stop('R is not a symmetric matrix.')
   }
+
   eigenvalues <- eigen(R, only.values = TRUE)$values
   eigenvalues[abs(eigenvalues) < 1e-08] <- 0
   if(any(eigenvalues < 0)){
     stop('R is not a positive semidefinite matrix.')
   }
 
+  if(length(shat) == 1){
+    shat = rep(shat, length(bhat))
+  }
+  if(length(bhat) != length(shat)){
+    stop('The length of bhat does not agree with length of shat.')
+  }
+
+  X0 = diag(R) == 0
   # convert to correlation matrix
-  R = cov2cor(R)
+  # if R has 0 colums and rows, cov2cor produces NaN and warning
+  R = muffled_cov2cor(R)
+  # change the columns and rows with NaN to 0
+  if(sum(X0) > 0){
+    R[X0, ] = R[,X0] = 0
+  }
 
   that = bhat/shat
 
--- tests/testthat/test_susie_ss_bhat.R
+++ tests/testthat/test_susie_ss_bhat.R
@@ -7,18 +7,17 @@ test_that("Results from ss bhat interface vs original data: no standardize",{
   R = cor(X)
 
   orig = susie(X, y, intercept = TRUE, standardize = FALSE, max_iter = 2,
-              estimate_residual_variance=FALSE, estimate_prior_variance = TRUE)
+              estimate_residual_variance=FALSE, estimate_prior_variance = FALSE)
 
-  fit = susie_bhat(bhat = ss$betahat, shat = ss$sebetahat, R = R, R_type = 'Cor',
+  fit = susie_bhat(bhat = ss$betahat, shat = ss$sebetahat, R = R,
                    var_y = var(y), n = n, standardize = FALSE,
-                   max_iter = 2, estimate_prior_variance = TRUE)
+                   max_iter = 2, estimate_prior_variance = FALSE)
 
   expect_equal(fit$alpha, orig$alpha)
   expect_equal(fit$mu, orig$mu)
   expect_equal(fit$mu2, orig$mu2)
   expect_equal(fit$V, orig$V)
-  cm = colMeans(X, na.rm = TRUE)
-  X.c = t(t(X) - cm)
+  X.c = safe_colScale(X, center = TRUE, scale = FALSE)
   expect_equal(crossprod(X.c, orig$fitted), fit$Xtfitted)
 })
 
@@ -26,24 +25,41 @@ test_that("Results from ss bhat interface vs original data: standardize",{
   simulate(200,1000)
   ss = univariate_regression(X, y)
   R = cor(X)
-  cm = colMeans(X, na.rm = TRUE)
-  csd = matrixStats::colSds(X, center = cm)
-  csd[csd==0] = 1
-  X.s = t(t(X)/csd)
 
-  orig = susie(X.s, y/sd(y), intercept = TRUE, standardize = FALSE, max_iter = 2,
-               estimate_residual_variance=FALSE, estimate_prior_variance = TRUE)
+  X.s = safe_colScale(X, center = FALSE, scale = TRUE)
+  X.cs = safe_colScale(X, center = TRUE, scale = TRUE)
+  orig = susie(X.s, y/sd(y), intercept = TRUE, standardize = TRUE, max_iter = 2,
+               estimate_residual_variance=FALSE, estimate_prior_variance = FALSE)
 
-  fit = susie_bhat(bhat = ss$betahat, shat = ss$sebetahat, R = R, R_type = 'Cor',
-                   var_y = 1, n = n, standardize = TRUE,
-                   max_iter = 2, estimate_prior_variance = TRUE)
+  fit = susie_bhat(bhat = ss$betahat, shat = ss$sebetahat, R = R,
+                   n = n, standardize = TRUE,
+                   max_iter = 2, estimate_prior_variance = FALSE)
 
   expect_equal(fit$alpha, orig$alpha)
   expect_equal(fit$mu, orig$mu)
   expect_equal(fit$mu2, orig$mu2)
   expect_equal(fit$V, orig$V)
-
-  X.cs = t((t(X) - cm)/csd)
   expect_equal(crossprod(X.cs, orig$fitted), fit$Xtfitted)
 })
 
+test_that("Results from ss bhat interfaceL: z scores",{
+  simulate(200,1000)
+  ss = univariate_regression(X, y)
+  R = cor(X)
+
+  X.s = safe_colScale(X, center = FALSE, scale = TRUE)
+  X.cs = safe_colScale(X, center = TRUE, scale = TRUE)
+
+  orig = susie(X.s, y/sd(y), intercept = TRUE, standardize = TRUE, max_iter = 2,
+               estimate_residual_variance=FALSE, estimate_prior_variance = FALSE)
+
+  fit = susie_bhat(bhat = ss$betahat/ss$sebetahat, shat = 1, R = R,
+                   n = n, standardize = TRUE,
+                   max_iter = 2, estimate_prior_variance = FALSE)
+
+  expect_equal(fit$alpha, orig$alpha)
+  expect_equal(fit$mu, orig$mu)
+  expect_equal(fit$mu2, orig$mu2)
+  expect_equal(fit$V, orig$V)
+  expect_equal(crossprod(X.cs, orig$fitted), fit$Xtfitted)
+})
