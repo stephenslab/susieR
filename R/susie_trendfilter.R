6cd0c4772b7a1b143f5c67fef34dfc45669715af
Gao Wang <gaow@uchicago.edu>
2018-10-29 16:41:04 -0500

    Fix tf example

--- R/susie_trendfilter.R
+++ R/susie_trendfilter.R
@@ -5,6 +5,7 @@
 #' @param order a scalar for the order of trend filtering
 #' @return SuSiE fit for trend filtering
 #' @examples
+#' y = c(rep(0,5),rep(1,5),rep(3,5),rep(-2,5),rep(0,30)) + rnorm(50)
 #' susie_trendfilter(y, 0)
 #' susie_trendfilter(y, 1, L=20)
 #' susie_trenndfilter(y, 0, estimate_prior_variance = TRUE)
@@ -16,4 +17,3 @@ susie_trendfilter = function(Y, order, ...){
   s = susie(X=X, Y=Y, ...)
   return(s)
 }
-
