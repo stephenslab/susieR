0f103c00bdd14ab02dede7174637336400794eb9
kaiqian <kaiqianz@uchicago.edu>
2019-01-18 13:40:09 -0600

    add a unit test for est_V investigating both V\neq 0 and V=0 scenarios

---
+++ tests/testthat/test_est_V.R
@@ -0,0 +1,15 @@
+context("test_est_V.R")
+
+test_that("est_V has V nonzero for the debug case", {
+  debug = readRDS('est_V_debug.rds')
+  betahat = debug$b
+  shat2 = debug$s2
+  prior_weights = debug$pw
+  V_debug = est_V(betahat, shat2, prior_weights)
+
+  betahat_V0 = rep(0, 512)
+  shat2_V0  = rep(1, 512)
+  V_0 = est_V(betahat_V0, shat2_V0, prior_weights)
+  expect_false(V_debug==0)
+  expect_equal(V_0, 0)
+})
