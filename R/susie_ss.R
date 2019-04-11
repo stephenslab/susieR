04e16ef576223ddc01509a030802b4f06e0d309c
zouyuxin <yuxin.zou.stat@gmail.com>
2019-04-11 10:56:27 -0500

    add functions to check XtX and Xty; update documentation

--- R/susie_ss.R
+++ R/susie_ss.R
@@ -1,29 +1,29 @@
 #' @title Bayesian sum of single-effect (susie) linear regression using summary stat
-#' @details Performs sum of single-effect (susie) linear regression of Y on X when
+#' @details Performs sum of single-effect (susie) linear regression of y on X when
 #' only summary statistics are available. The summary data required are
-#' the p by p matrix X'X, the p vector X'Y, and the sample variance of Y, or (1/n)Y'Y. The summary stats should come from the same individuals.
-#' Both the columns of X and the vector Y
+#' the p by p matrix X'X, the p vector X'y, the sum of squares of y (y'y) and the sample size.
+#' The summary stats should come from the same individuals.
+#' Both the columns of X and the vector y
 #' should be centered to have mean 0 before
-#' computing these summary statistics; you may also want to scale each column of X and Y to have variance 1 (see examples).
-#' This function fits the regression model Y = sum_l Xb_l + e, where elements of e are iid N(0,var=residual_variance) and the
+#' computing these summary statistics; you may also want to scale each column of X and y to have variance 1 (see examples).
+#' This function fits the regression model y = sum_l Xb_l + e, where elements of e are iid N(0,var=residual_variance) and the
 #' sum_l b_l is a p vector of effects to be estimated.
 #' The assumption is that each b_l has exactly one non-zero element, with all elements
-#' equally likely to be non-zero. The prior on the non-zero element is N(0,var=scaled_prior_variance*var(Y)).
+#' equally likely to be non-zero. The prior on the non-zero element is N(0,var=scaled_prior_variance*y'y/(n-1)).
 #' @param XtX a p by p matrix, X'X, where columns of X are centered to have mean 0
-#' @param Xty a p vector, X'Y, where columns of X are centered and Y is centered to have mean 0
-#' @param yty a scaler, Y'Y, where Y is centered to have mean 0
+#' @param Xty a p vector, X'y, where columns of X are centered and y is centered to have mean 0
+#' @param yty a scaler, y'y, where y is centered to have mean 0
 #' @param n sample size
 #' @param L maximum number of non-zero effects
-#' @param type sufficient or z scores
-#' @param scaled_prior_variance the scaled prior variance (vector of length L, or scalar. In latter case gets repeated L times )
-#' @param residual_variance the residual variance (defaults to variance of Y)
+#' @param scaled_prior_variance the scaled prior variance (vector of length L, or scalar. In latter case gets repeated L times)
+#' @param residual_variance the residual variance (defaults to variance of y)
 #' @param estimate_residual_variance indicates whether to estimate residual variance
 #' @param estimate_prior_variance indicates whether to estimate prior
 #' @param estimate_prior_method The method used for estimating prior variance, 'optim' or 'EM'
 #' @param r_tol tolerance level for eigen value check of positive semidefinite matrix of R.
 #' @param prior_weights a p vector of prior probability that each element is non-zero
 #' @param null_weight probability of no effect, for each single effect model
-#' @param standardize logical flag (default=TRUE) for whether to standardize columns of X to unit variance prior to fitting.
+#' @param standardize logical flag (default=TRUE) for whether to adjust XtX and Xty such that they are computed from column standardized X, prior to fitting.
 #' Note that `scaled_prior_variance` specifies the prior on the coefficients of X *after* standardization (if performed).
 #' If you do not standardize you may need
 #' to think more carefully about specifying
@@ -38,7 +38,12 @@
 #' @param tol convergence tolerance based on alpha
 #' @param verbose if true outputs some progress messages
 #' @param track_fit add an attribute \code{trace} to output that saves current values of all iterations
-#' @param skip_checks whether to skip the checks for XtX and Xty
+#' @param check_input whether to perform checks on XtX and Xty, the checks are:
+#'
+#' 1. Check whether XtX is positive semidefinite
+#'
+#' 2. Check whether Xty in space spanned by the non-zero eigenvectors of XtX
+#'
 #' @return a susie fit, which is a list with some or all of the following elements\cr
 #' \item{alpha}{an L by p matrix of posterior inclusion probabilites}
 #' \item{mu}{an L by p matrix of posterior means (conditional on inclusion)}
@@ -63,21 +68,41 @@
 susie_ss = function(XtX, Xty, yty, n, L=10,
                     scaled_prior_variance=0.2,
                     residual_variance=NULL,
-                    r_tol = 1e-08,
-                    prior_weights = NULL, null_weight = NULL,
-                    standardize = TRUE,
                     estimate_residual_variance = TRUE,
                     estimate_prior_variance = TRUE,
                     estimate_prior_method = c("optim","EM"),
+                    r_tol = 1e-08,
+                    prior_weights = NULL, null_weight = NULL,
+                    standardize = TRUE,
                     max_iter=100,s_init = NULL, intercept_value=0,
                     coverage=0.95, min_abs_corr=0.5,
-                    tol=1e-3, verbose=FALSE, track_fit = FALSE, skip_checks=FALSE){
+                    tol=1e-3, verbose=FALSE, track_fit = FALSE, check_input=FALSE){
   # Process input estimate_prior_method.
   estimate_prior_method <- match.arg(estimate_prior_method)
 
   # Check input XtX.
+  if(ncol(XtX) != length(Xty)) {
+    stop(paste0('The dimension of XtX (', nrow(XtX), ' by ', ncol(XtX), ') does not agree with expected (', length(Xty), ' by ', length(Xty), ')'))
+  }
+  if(!is_symmetric_matrix(XtX)){
+    stop('XtX is not a symmetric matrix.')
+  }
   if (!(is.double(XtX) & is.matrix(XtX)) & !inherits(XtX,"CsparseMatrix"))
     stop("Input X must be a double-precision matrix, or a sparse matrix.")
+  if(any(is.na(XtX)))
+    stop('XtX matrix contains NA.')
+  if(check_input){
+    # Check whether XtX is positive semidefinite
+    semi_pd = check_semi_pd(XtX, r_tol)
+    if(semi_pd$status == FALSE){
+      stop('XtX is not a positive semidefinite matrix.')
+    }
+    # Check whether Xty in space spanned by the non-zero eigenvectors of XtX
+    proj = check_projection(semi_pd$matrix, Xty)
+    if(proj$status == FALSE)
+      warning('Xty does not lie in the space of non-zero eigenvectors of XtX')
+  }
+
   if (is.numeric(null_weight) && null_weight == 0) null_weight = NULL
   if (!is.null(null_weight)) {
     if (!is.numeric(null_weight))
@@ -105,18 +130,6 @@ susie_ss = function(XtX, Xty, yty, n, L=10,
   }
   attr(XtX, "d") <- diag(XtX)
   attr(XtX, "scaled:scale") <- csd
-
-  attr(XtX, 'eigen') <- eigen(XtX, symmetric = TRUE)
-  # attr(XtX, 'eigen')$values[abs(attr(XtX, 'eigen')$values) < r_tol] <- 0
-
-  # check whether Xty in space spanned by the non-zero eigenvectors of XtX
-  if(!skip_checks){
-    A = attr(XtX, 'eigen')$vectors[,attr(XtX, 'eigen')$values!=0]
-    in_space = all.equal(as.vector(A %*% crossprod(A, Xty)), Xty)
-    if(in_space!=TRUE){
-      warning('Xty does not lie in the space of non-zero eigenvectors of XtX')
-    }
-  }
 
   # initialize susie fit
   s = init_setup(0,p,L,scaled_prior_variance,residual_variance,
@@ -194,81 +207,57 @@ susie_ss = function(XtX, Xty, yty, n, L=10,
   return(s)
 }
 
-#' @title Check input covariance / correlation matrix
-#' @param R a p by p matrix of X'X, covariance matrix or correlation matrix
-#' @return a verified matrix
-#' @keywords internal
+#' @title Check whether A is positive semidefinite
+#' @param A a symmetric matrix
+#' @return a list of result: \cr
+#' \item{matrix}{The matrix with eigen decomposition}
+#' \item{status}{whether A is positive semidefinite}
+#' @export
+check_semi_pd <- function(A, tol){
+  attr(A, 'eigen') = eigen(A, symmetric = TRUE)
 
-check_r_matrix <- function(R, expected_dim, r_tol) {
-  n = nrow(R)
-  if(n != expected_dim) {
-    stop(paste0('The dimension of R (', n, ' by ', n, ') does not agree with expected (', expected_dim, ' by ', expected_dim, ')'))
-  }
-  if(!is_symmetric_matrix(R)){
-    stop('R is not a symmetric matrix.')
-  }
+  eigenvalues = attr(A, 'eigen')$values
+  eigenvalues[eigenvalues < tol] <- 0
 
-  E <- tryCatch(suppressWarnings(chol(R, pivot = TRUE, tol=r_tol)),error = function(e) FALSE)
-  if (is.logical(E)) {
-    stop('R is not a positive semidefinite matrix.')
-  }
+  # E <- tryCatch(suppressWarnings(chol(R, pivot = TRUE, tol=r_tol)),error = function(e) FALSE)
+  # if (is.logical(E)) {
+  #   stop('R is not a positive semidefinite matrix.')
+  # }
 
-  if(any(is.na(R))){
-    warning('R matrix contains NA, it is replaced with 0.')
-    isnaR = is.na(R)
-    naind = which(rowSums(isnaR) == ncol(R)-1)
-    R[,naind] = 0
-    R[naind,] = 0
-    R[isnaR] = 0
-  }
-  X0 = diag(R) == 0
-  # convert any input R to correlation matrix
-  # if R has 0 colums and rows, cov2cor produces NaN and warning
-  R = muffled_cov2cor(R)
-  # change the columns and rows with NaN to 0
-  if(sum(X0) > 0){
-    R[X0, ] = R[,X0] = 0
-  }
-  return(R)
+  return(list(matrix = A, status = !any(eigenvalues < 0)))
+}
+
+#' @title Check whether b in space spanned by the non-zero eigenvectors of A
+#' @param A a p by p matrix
+#' @param b a length p vector
+#' @return a list of result: \cr
+#' \item{status}{whether b in space spanned by the non-zero eigenvectors of A}
+#' \item{msg}{msg gives the difference between the projected b and b if status is FALSE}
+#' @export
+check_projection <- function(A, b){
+  if(is.null(attr(A, 'eigen')))
+    attr(A, 'eigen') = eigen(A, symmetric = TRUE)
+
+  B = attr(A, 'eigen')$vectors[,attr(A, 'eigen')$values!=0]
+  msg = all.equal(as.vector(B %*% crossprod(B, b)), b)
+
+  if (length(msg)==1 && msg == TRUE)
+    return(list(status=T, msg=NA))
+  else
+    return(list(status=F,msg=msg))
 }
 
 #' @title Summary statistics version of SuSiE on betahat, the corresponding standard error, and correlation (or covariance) matrix
 #' @param bhat a p vector of estimated effects.
 #' @param shat a p vector of corresponding standard errors.
-#' @param R a p by p symmetric and positive semidefinite matrix. It can be X'X, covariance matrix or correlation matrix.
+#' @param R a p by p symmetric and positive semidefinite matrix. It can be X'X, covariance matrix (X'X/(n-1)) or correlation matrix.
 #' @param n sample size.
-#' @param var_y the (sample) variance of y. If it is unknown, the coefficients (returned from `coef`) are on the standardized X, y scale.
-#' @param r_tol tolerance level for eigen value check of positive semidefinite matrix of R.
-#' @param L maximum number of non-zero effects.
-#' @param scaled_prior_variance the scaled prior variance (vector of length L, or scalar. In latter case gets repeated L times)
-#' @param estimate_residual_variance indicates whether to estimate residual variance
-#' @param estimate_prior_variance indicates whether to estimate prior
-#' @param estimate_prior_method The method used for estimating prior variance, 'optim' or 'EM'
-#' @param prior_weights a p vector of prior probability that each element is non-zero
-#' @param null_weight probability of no effect, for each single effect model
-#' @param standardize logical flag (default=TRUE) for whether to standardize columns of X to unit variance prior to fitting. It is useful when `var_y` is given.
-#' Note that `scaled_prior_variance` specifies the prior on the coefficients of X *after* standardization (if performed).
-#' If you do not standardize you may need
-#' to think more carefully about specifying `scaled_prior_variance`.
-#' Whatever the value of standardize, the coefficients (returned from `coef`) are on the bhat, shat scale.
-#' @param coverage coverage of confident sets. Default to 0.95 for 95\% credible interval.
-#' @param min_abs_corr minimum of absolute value of correlation allowed in a credible set.
-#' @param verbose if true outputs some progress messages
-#' @param track_fit add an attribute \code{trace} to output that saves current values of all iterations
+#' @param var_y the (sample) variance of y, defined as y'y/(n-1) . If it is unknown, the coefficients (returned from `coef`) are on the standardized X, y scale.
 #' @param ... further arguments to be passed to \code{\link{susie_ss}}
 #' @return a susie fit
 #'
 #' @export
-susie_bhat = function(bhat, shat, R, n, var_y = 1, r_tol = 1e-08,
-                      L=10,
-                      scaled_prior_variance=0.2,
-                      estimate_residual_variance = TRUE,
-                      estimate_prior_variance = TRUE,
-                      estimate_prior_method = c("optim", "EM"),
-                      prior_weights = NULL, null_weight = NULL,
-                      standardize = TRUE,
-                      coverage=0.95, min_abs_corr=0.5,
-                      verbose=FALSE, track_fit = FALSE, ...){
+susie_bhat = function(bhat, shat, R, n, var_y = 1, ...){
   if(missing(n)) {
     stop('n must be provided')
   }
@@ -286,8 +275,16 @@ susie_bhat = function(bhat, shat, R, n, var_y = 1, r_tol = 1e-08,
   that[is.na(that)] = 0
   R2 = that^2/(that^2 + n-2)
   sigma2 = (n-1)*(1-R2)/(n-2)
-  #
-  R = check_r_matrix(R, length(bhat), r_tol)
+
+  # convert any input R to correlation matrix
+  # if R has 0 colums and rows, cov2cor produces NaN and warning
+  X0 = diag(R) == 0
+  R = muffled_cov2cor(R)
+  # change the columns and rows with NaN to 0
+  if(sum(X0) > 0){
+    R[X0, ] = R[,X0] = 0
+  }
+
   #
   if(missing(var_y)) {
     XtX = (n-1)*R
@@ -298,16 +295,7 @@ susie_bhat = function(bhat, shat, R, n, var_y = 1, r_tol = 1e-08,
     XtX = t(R * sqrt(XtXdiag)) * sqrt(XtXdiag)
   }
 
-  estimate_prior_method = match.arg(estimate_prior_method)
-  susie_ss(XtX = XtX, Xty = Xty, yty = var_y * (n-1), n = n, L = L,
-           scaled_prior_variance = scaled_prior_variance,
-           estimate_residual_variance = estimate_residual_variance,
-           estimate_prior_variance = estimate_prior_variance,
-           estimate_prior_method = estimate_prior_method,
-           prior_weights = prior_weights, null_weight = null_weight,
-           standardize = standardize,
-           coverage=coverage, min_abs_corr=min_abs_corr,
-           verbose=verbose, track_fit = track_fit, ...)
+  susie_ss(XtX = XtX, Xty = Xty, yty = var_y * (n-1), n = n, ...)
 }
 
 
