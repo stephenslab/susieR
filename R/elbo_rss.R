364819e33e0b4dec603b5b9efa7e009d3081af32
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-06-30 22:12:47 -0500

    More updates to the roxygen2 docs.

--- R/elbo_rss.R
+++ R/elbo_rss.R
@@ -1,29 +1,19 @@
-#' @title Get objective function from data and susie fit object.
-#'
-#' @param R p by p corelation matrix
-#' @param z length p vector
-#' @param s a susie fit object
-#' @keywords internal
-get_objective_rss = function(R, z, s) {
+# @title Get objective function from data and susie fit object.
+# @param R p by p corelation matrix
+# @param z length p vector
+# @param s a susie fit object
+get_objective_rss = function(R, z, s) 
   return(Eloglik_rss(R,z,s)-sum(s$KL))
-}
 
 # @title expected loglikelihood for a susie fit
 Eloglik_rss = function(R,z,s){
   d = s$sigma2 * attr(R, 'eigen')$values + attr(R, 'lambda')
-
-  if(attr(R, 'lambda') == 0){
+  if(attr(R, 'lambda') == 0)
     result =  -(sum(d!=0)/2) * log(2*pi*s$sigma2) -
       0.5 * get_ER2_rss(R,z,s)
-  }else{
-    # Positive = d > 1e-8
-    # d[!Positive] = 0
-    # result =  -(sum(d!=0)/2) * log(2*pi) - 0.5*sum(log(d[Positive])) -
-    #   0.5 * get_ER2_rss(R,z,s)
-
+  else
     result =  -(length(z)/2) * log(2*pi) - 0.5*sum(log(d)) -
       0.5 * get_ER2_rss(R,z,s)
-  }
   return(result)
 }
 
@@ -32,46 +22,38 @@ Eloglik_rss = function(R,z,s){
 get_ER2_rss = function(R,z,s){
 
   d = s$sigma2 * attr(R, 'eigen')$values + attr(R, 'lambda')
-
-  # Positive = d > 1e-8
-  # d[!Positive] = 0
   Dinv = 1/d
   Dinv[is.infinite(Dinv)] = 0
-  SinvR = attr(R, 'eigen')$vectors %*% ((Dinv*attr(R, 'eigen')$values) * t(attr(R, 'eigen')$vectors))
+  SinvR = attr(R, 'eigen')$vectors %*% ((Dinv*attr(R, 'eigen')$values) *
+          t(attr(R, 'eigen')$vectors))
   Utz = crossprod(attr(R, 'eigen')$vectors, z)
   zSinvz = sum( Utz * (Dinv * Utz))
-  # zSinvz = sum(z * (attr(R, 'eigen')$vectors %*% (Dinv * crossprod(attr(R, 'eigen')$vectors, z))))
 
   Z = s$alpha*s$mu
-  if(attr(R, 'lambda') == 0){
+  if(attr(R, 'lambda') == 0)
     RSinvR = R/s$sigma2
-  }else{
+  else
     RSinvR = R %*% SinvR
-  }
   RZ2 = sum((Z%*%RSinvR) * Z)
 
   zbar = colSums(Z)
-
   postb2 = s$alpha * s$mu2 #posterior second moment
-
-  return(zSinvz - 2*sum( (SinvR %*% z) * zbar) + sum(zbar * (RSinvR %*% zbar)) -
-           RZ2 + sum(diag(RSinvR)*t(postb2)))
+  return(zSinvz - 2*sum( (SinvR %*% z) * zbar)
+         + sum(zbar * (RSinvR %*% zbar))
+         - RZ2 + sum(diag(RSinvR)*t(postb2)))
 }
 
-
-#' @title posterior expected loglikelihood for a single effect regression
-#' @param R a p by p symmetric and positive semidefinite correlation matrix.
-#' @param Sigma residual_var * R + lambda I
-#' @param r residuals
-#' @param Eb the posterior mean of b (p vector) (alpha * mu)
-#' @param Eb2 the posterior second moment of b (p vector) (alpha * mu2)
-#' @keywords internal
+# @title posterior expected loglikelihood for a single effect regression
+# @param R a p by p symmetric and positive semidefinite correlation matrix.
+# @param Sigma residual_var * R + lambda I
+# @param r residuals
+# @param Eb the posterior mean of b (p vector) (alpha * mu)
+# @param Eb2 the posterior second moment of b (p vector) (alpha * mu2)
 SER_posterior_e_loglik_rss = function(R, Sigma, r,Ez,Ez2){
   eigenS = attr(Sigma,'eigenS')
   Dinv = 1/(eigenS$values)
   Dinv[is.infinite(Dinv)] = 0
   rR = R %*% r
   SinvEz = eigenS$vectors %*% (Dinv * crossprod(eigenS$vectors, Ez))
-
-  - (0.5) * (- 2*sum(rR*SinvEz) + sum(attr(Sigma, 'RjSinvRj')*as.vector(Ez2)))
+    - (0.5)*(- 2*sum(rR*SinvEz) + sum(attr(Sigma, 'RjSinvRj')*as.vector(Ez2)))
 }
--- R/estimate_residual_variance.R
+++ R/estimate_residual_variance.R
@@ -1,21 +1,18 @@
-#' @title Estimate residual variance
-#' @param X an n by p matrix of covariantes
-#' @param Y an n vector of data
-#' @param s a susie fit
-#' @keywords internal
+# @title Estimate residual variance
+# @param X an n by p matrix of covariantes
+# @param Y an n vector of data
+# @param s a susie fit
 estimate_residual_variance = function(X,Y,s){
   n = nrow(X)
   return( (1/n)* get_ER2(X,Y,s) )
 }
 
-
-#' @title Estimate residual variance for summary statistics
-#' @param XtX a p by p matrix
-#' @param Xty a p vector
-#' @param s a susie fit
-#' @param yty a scaler, Y'Y, where Y is centered to have mean 0
-#' @param n sample size
-#' @keywords internal
+# @title Estimate residual variance for summary statistics
+# @param XtX a p by p matrix
+# @param Xty a p vector
+# @param s a susie fit
+# @param yty a scaler, Y'Y, where Y is centered to have mean 0
+# @param n sample size
 estimate_residual_variance_ss = function(XtX,Xty,s,yty,n){
   return( (1/n)* get_ER2_ss(XtX,Xty,s,yty) )
 }
