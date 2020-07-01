b9f0fa53a279aaaa53ea98a2d44f16311881551f
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-06-30 22:01:01 -0500

    Revised roxygen2 docs for a few functions.

--- R/compute_ss.R
+++ R/compute_ss.R
@@ -1,22 +1,28 @@
 #' @title Compute sufficient statistics from input data
-#' @param X an n by p matrix of covariates
-#' @param y an n vector
-#' @param standardize logical flag (default=TRUE) for whether to standardize columns of X to unit variance prior to fitting.
-#' @return a list of sufficient statistics
+#' 
+#' @param X An n by p matrix of covariates.
+#' 
+#' @param y An n vector.
+#' 
+#' @param standardize Logical flag indicating whether to standardize
+#'   columns of X to unit variance prior to fitting.
+#' 
+#' @return A list of sufficient statistics.
+#' 
 #' @importFrom methods as
+#' 
 #' @export
+#' 
 compute_ss = function(X, y, standardize = TRUE){
   y = y - mean(y)
   is.sparse = !(is.matrix(X))
   X = set_X_attributes(as.matrix(X), center=TRUE, scale = standardize)
   X = t((t(X) - attr(X, 'scaled:center'))/attr(X, 'scaled:scale'))
   XtX = crossprod(X)
-  if(is.sparse){
+  if(is.sparse)
     XtX = as(XtX,"dgCMatrix")
-  }
   Xty = c(y %*% X)
   n = length(y)
   yty = sum(y^2)
-
   return(list(XtX = XtX, Xty = Xty, yty = yty, n = n))
 }
--- R/predict.susie.R
+++ R/predict.susie.R
@@ -1,32 +1,53 @@
-#' @title extract regression coefficients from susie fit
-#' @param object a susie fit
-#' @return a p+1 vector, the first element being an intercept, and the remaining p elements being estimated regression coefficients
+#' @title Extract regression coefficients from susie fit
+#' 
+#' @param object A susie fit.
+#'
+#' @param \dots Additional arguments passed to the default S3 method.
+#' 
+#' @return A p+1 vector, the first element being an intercept, and the
+#'   remaining p elements being estimated regression coefficients.
+#'
+#' @importFrom stats coef
+#' 
 #' @export coef.susie
+#' 
 #' @export
-coef.susie = function(object, ...){
+#' 
+coef.susie = function(object, ...) {
   s <- object
   c(s$intercept,colSums(s$alpha*s$mu)/s$X_column_scale_factors)
 }
 
-#' @title predict future observations or extract coefficients from susie fit
-#' @param object a susie fit
-#' @param newx a new value for X at which to do predictions
-#' @param type if this is coefficients, then calls coef.susie
+#' @title Predict outcomes or extract coefficients from susie fit.
+#' 
+#' @param object A susie fit.
+#' 
+#' @param newx A new value for X at which to do predictions.
+#' 
+#' @param type The type of output. For \code{type = "response"},
+#'   predicted or fitted outcomes are returned; for \code{type =
+#'   "coefficients"}, the estimated coefficients are returned.
+#'
+#' @return For \code{type = "response"}, predicted or fitted outcomes
+#'   are returned; for \code{type = "coefficients"}, the estimated
+#'   coefficients are returned.
+#' 
 #' @importFrom stats coef
+#' 
 #' @export predict.susie
+#' 
 #' @export
+#' 
 predict.susie = function(object,newx = NULL,
-                         type=c("response","coefficients"),...) {
-  s <- object
+                         type = c("response","coefficients"),...) {
+  s    <- object
   type <- match.arg(type)
   if (type=="coefficients"){
-    if(!missing(newx)){
+    if(!missing(newx))
       stop("Do not supply newx when predicting coefficients")
-    }
     return(coef(s))
   }
-
-  if(missing(newx)){return(s$fitted)}
-
+  if(missing(newx))
+    return(s$fitted)
   return(drop(s$intercept + newx %*% coef(s)[-1]))
 }
