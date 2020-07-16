3a4aa3f5d53c59af89a87def969521910987231c
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-07-16 18:33:46 -0500

    Some minor fixes to susie_plot_iteration.

--- R/susie_plots.R
+++ R/susie_plots.R
@@ -8,15 +8,25 @@
 #' It has to contain `z`, `PIP` and optionally `sets`.
 #' It is also possible to take in a vector of z-score or PIP,
 #' in order to plot data from other software program.
-#' @param y a string indicating what to plot: z (for z-score), PIP, log10PIP
-#' or a random label to plot input data as is.
+#' 
+#' @param y a string indicating what to plot: z (for z-score), PIP,
+#' log10PIP or a random label to plot input data as is.
+#' 
 #' @param add_bar add horizontal bar to signals in credible interval.
-#' @param pos can be either 1) numeric vector of indices of variables to plot, default to all variables, or
-#' 2) a list of \code{list(attr = , start = , end = )} where \code{attr} is a character string of the name of 
-#' index variable in \code{model} object, \code{start} and \code{end} are boundaries of indices to plot.
+#' 
+#' @param pos can be either 1) numeric vector of indices of variables
+#' to plot, default to all variables, or 2) a list of \code{list(attr
+#' = , start = , end = )} where \code{attr} is a character string of
+#' the name of index variable in \code{model} object, \code{start} and
+#' \code{end} are boundaries of indices to plot.
+#' 
 #' @param b for simulated data, specify b = true effects (highlights in red).
-#' @param max_cs the biggest CS to display, based on purity (set max_cs in between 0 and 1) or size (>1).
-#' @param add_legend if TRUE, add a legend to annotate the size and purity of each CS discovered.
+#' 
+#' @param max_cs the biggest CS to display, based on purity (set
+#' max_cs in between 0 and 1) or size (>1).
+#' 
+#' @param add_legend if TRUE, add a legend to annotate the size and
+#' purity of each CS discovered.
 #'
 #' @importFrom utils head
 #' @importFrom stats pnorm
@@ -45,7 +55,7 @@ susie_plot = function(model,y,add_bar=FALSE,pos=NULL,b=NULL,max_cs=400,add_legen
     "darkturquoise", "green1", "yellow4", "yellow3",
     "darkorange4", "brown"
   )
-  if (y=="z") {
+  if (y == "z") {
     if (is_susie) {
       if (is.null(model$z))
         stop("z-score not available from SuSiE fit. Please set `compute_univariate_zscore=TRUE` in `susie()` function call.")
@@ -78,6 +88,7 @@ susie_plot = function(model,y,add_bar=FALSE,pos=NULL,b=NULL,max_cs=400,add_legen
     if (pos$start>=pos$end) stop("Position start should be smaller than end")
     start = min(min(model[[pos$attr]]), pos$start)
     end = max(max(model[[pos$attr]]), pos$end)
+    
     # add zeros to alpha and p
     alpha = matrix(0, nrow(model$alpha), end - start + 1)
     new_p = rep(min(p), end - start + 1)
@@ -148,62 +159,66 @@ susie_plot = function(model,y,add_bar=FALSE,pos=NULL,b=NULL,max_cs=400,add_legen
 #' @rdname susie_plots
 #' 
 #' @details Diagnostic plot for SuSiE iterations
+#' 
 #' @param model a susie fit, the output of `susieR::susie()`.
-#' Multiple plots will be made for all iterations if `track_fit` was set to `TRUE` when running SuSiE.
+#' Multiple plots will be made for all iterations if `track_fit` was
+#' set to `TRUE` when running SuSiE.
+#' 
 #' @param L an integer, number of CS to plot
+#' 
 #' @param file_prefix prefix to path of output plot file
+#' 
 #' @param pos index of variables to plot, default to all variables
 #' 
 #' @importFrom grDevices pdf
 #' @importFrom grDevices dev.off
 #' @importFrom reshape melt
 #' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes_string
+#' @importFrom ggplot2 geom_col
+#' @importFrom ggplot2 ggtitle
+#' @importFrom ggplot2 theme_classic
 #' 
 #' @export
 #' 
-susie_plot_iteration = function (model, L, file_prefix, pos=NULL) {
-  if(!requireNamespace("ggplot2",quietly = TRUE))
-    stop("Required package ggplot2 not found")
-  if(!requireNamespace("reshape",quietly = TRUE))
-    stop("Required package reshape not found")
-  get_layer = function(obj, k, idx, vars) {
+susie_plot_iteration = function (model, L, file_prefix, pos = NULL) {
+  get_layer = function (obj, k, idx, vars) {
     alpha = melt(obj$alpha[1:k,vars,drop=FALSE])
     colnames(alpha) = c("L","variables","alpha")
     alpha$L = as.factor(alpha$L)
-    ggplot2::ggplot(alpha,ggplot2::aes_string("variables","alpha",group="L")) +
-      ggplot2::geom_col(ggplot2::aes_string(fill = "L")) +
-      ggplot2::ggtitle(paste("Iteration", idx)) +
-      ggplot2::theme_classic()
+    ggplot(alpha,aes_string("variables","alpha",group = "L")) +
+      geom_col(aes_string(fill = "L")) +
+      ggtitle(paste("Iteration",idx)) +
+      theme_classic()
   }
-  k = min(nrow(model$alpha), L)
+  k = min(nrow(model$alpha),L)
   if (is.null(pos))
     vars = 1:ncol(model$alpha)
   else
     vars = pos
-  pdf(paste0(file_prefix,".pdf"), 8, 3)
-  if (is.null(model$trace)) {
+  pdf(paste0(file_prefix,".pdf"),8,3)
+  if (is.null(model$trace))
     print(get_layer(model, k, model$niter, vars))
-  } else {
-    for (i in 2:length(model$trace)) {
-      print(get_layer(model$trace[[i]], k, i-1, vars))
-    }
+  else {
+    for (i in 2:length(model$trace))
+      print(get_layer(model$trace[[i]],k,i-1,vars))
   }
   dev.off()
   format = ".pdf"
   if (!is.null(model$trace)) {
     cmd = paste("convert -delay 30 -loop 0 -density 300 -dispose previous",
                 paste0(file_prefix,".pdf"),
-                "\\( -clone 0 -set delay 300 \\) -swap 0 +delete \\( +clone -set delay 300 \\) +swap +delete -coalesce -layers optimize",
-                paste0(file_prefix, ".gif"))
+                "\\( -clone 0 -set delay 300 \\) -swap 0 +delete",
+                "\\( +clone -set delay 300 \\) +swap +delete -coalesce",
+                "-layers optimize",paste0(file_prefix,".gif"))
     cat("Creating GIF animation ...\n")
     if (file.exists(paste0(file_prefix,".gif")))
       file.remove(paste0(file_prefix,".gif"))
     output = try(system(cmd))
-    if (inherits(output,"try-error")) {
-      cat("Cannot create GIF animation because `convert` command failed.\n")
-    } else {
+    if (inherits(output,"try-error"))
+      cat("Cannot create GIF animation because convert command failed.\n")
+    else 
       format = ".gif"
-    }
   }
-  cat(paste0("Iterplot saved to ", file_prefix, format, "\n"))
+  cat(paste0("Iterplot saved to ",file_prefix,format,"\n"))
 }
