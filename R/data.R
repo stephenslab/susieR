d475994b2644bc31d8ccf49cf98a075cd7b7bec0
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-09-22 14:44:48 -0500

    Added scaffolding for data set help pages.

---
+++ R/data.R
@@ -0,0 +1,65 @@
+#' @name N2finemapping
+#'
+#' @title Add Title Here.
+#'
+#' @docType data
+#' 
+#' @description Briefly describe the data set here.
+#' 
+#' @format \code{N2finemapping} is a list with the following elements:
+#' 
+#' \describe{
+#'   \item{X}{Describe "X" here.}
+#' 
+#'   \item{chrom}{Describe "chrom" here.}
+#' 
+#'   \item{pos}{Describe "pos" here.}
+#'
+#'   \item{true_coef}{Describe "true_coef" here.}
+#'
+#'   \item{residual_variance}{Describe "residual_variance" here.}
+#'
+#'   \item{Y}{Describe "Y" here.}
+#'
+#'   \item{allele_freq}{Describe "allele_freq" here.}
+#'
+#'   \item{V}{Describe "V" here.}
+#' }
+#' 
+#' @keywords data
+#'
+NULL
+
+#' @name N3finemapping
+#'
+#' @title Add Title Here.
+#'
+#' @docType data
+#' 
+#' @description Briefly describe the data set here.
+#' 
+#' @format \code{N3finemapping} is a list with the following elements:
+#' 
+#' \describe{
+#'   \item{data$X}{Describe "X" here.}
+#' 
+#'   \item{data$chrom}{Describe "chrom" here.}
+#' 
+#'   \item{data$pos}{Describe "pos" here.}
+#'
+#'   \item{data$true_coef}{Describe "true_coef" here.}
+#'
+#'   \item{data$residual_variance}{Describe "residual_variance" here.}
+#'
+#'   \item{Y}{Describe "Y" here.}
+#'
+#'   \item{data$allele_freq}{Describe "allele_freq" here.}
+#'
+#'   \item{data$V}{Describe "V" here.}
+#'
+#'   \item{sumstats}{Describe "sumstats" here.}
+#' }
+#' 
+#' @keywords data
+#'
+NULL
