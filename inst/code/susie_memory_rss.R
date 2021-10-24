c4249df69470b76cf3b5ff28c27645ca8f98d014
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-10-24 10:48:08 -0500

    Improved the susie_memory.R script.

--- inst/code/susie_memory_rss.R
+++ inst/code/susie_memory_rss.R
@@ -1,21 +1,23 @@
 # export MEM_CHECK_INTERVAL=0.01
 # python3 monitor_memory.py Rscript susie_memory.R
-#
-# Without any improvements:
-# Size of X: 1 Gb
-# max vms_memory: 9.45GB
-#
 # library(susieR)
 devtools::load_all()
 set.seed(1)
-p <- 16000
-n <- 8000
+p <- 12000 # 8000
+n <- 4000 # 1000
 X <- matrix(rnorm(n*p),n,p)
 y <- rnorm(n)
+# ss <- susieR:::univariate_regression(X,y)
+# z <- ss$betahat/ss$sebetahat
+# R <- cor(X)
 cat("Size of X:\n")
 print(object.size(X),unit = "GB")
-cat("Running susie.\n")
+# cat("Size of R:\n")
+# print(object.size(R),unit = "GB")
+cat("Running susie_rss.\n")
 out <- susie(X,y,estimate_prior_variance = FALSE,min_abs_corr = 0,
              verbose = TRUE)
+# out <- susie_rss(z,R,estimate_prior_variance = FALSE,min_abs_corr = 0,
+#                  verbose = TRUE)
 print(sapply(out$sets$cs,length))
 
