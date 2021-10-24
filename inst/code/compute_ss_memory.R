76c8a7f649fd76415271cc24f5be5c3512fdc0e7
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-10-24 16:39:53 -0500

    Made a few improvements to the _memory.R scripts.

--- inst/code/compute_ss_memory.R
+++ inst/code/compute_ss_memory.R
@@ -6,16 +6,10 @@
 # - *Add notes here.*
 #
 # library(susieR)
-devtools::load_all()
-# set.seed(1)
-# p <- 16000
-# n <- 8000
-# X <- matrix(rnorm(n*p),n,p)
-# y <- rnorm(n)
-# save(list = c("X","y"),file = "susie_data.RData")
-load("susie_data.RData")
 set.seed(1)
+devtools::load_all()
+load("susie_rss_data.RData")
 cat("Size of X:\n")
 print(object.size(X),unit = "GB")
-cat("Running susie.\n")
+cat("Running compute_ss.\n")
 out <- compute_ss(X,y,standardize = TRUE)
--- inst/code/susie_rss_memory.R
+++ inst/code/susie_rss_memory.R
@@ -1,19 +1,26 @@
 # export MEM_CHECK_INTERVAL=0.01
 # python3 monitor_memory.py Rscript susie_rss_memory.R
+#
+# NOTES:
+#
+# - Add notes here.
+#
 # library(susieR)
 devtools::load_all()
-set.seed(1)
-p <- 8000
-n <- 1000
-X <- matrix(rnorm(n*p),n,p)
-y <- rnorm(n)
-ss <- susieR:::univariate_regression(X,y)
-z <- ss$betahat/ss$sebetahat
-R <- cor(X)
+# set.seed(1)
+# p  <- 8000
+# n  <- 1000
+# X  <- matrix(rnorm(n*p),n,p)
+# y  <- rnorm(n)
+# ss <- susieR:::univariate_regression(X,y)
+# z  <- ss$betahat/ss$sebetahat
+# R  <- cor(X)
+# save(list = c("X","y","z","R"),file = "susie_rss_data.RData")
+load("susie_rss_data.RData")
 cat("Size of R:\n")
 print(object.size(R),unit = "GB")
 cat("Running susie_rss.\n")
+set.seed(1)
 out <- susie_rss(z,R,estimate_prior_variance = FALSE,min_abs_corr = 0,
                  verbose = TRUE)
 print(sapply(out$sets$cs,length))
-
