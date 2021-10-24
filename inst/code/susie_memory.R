08d33f63a29352fa2ac4c20d3592fd7696fd3cb8
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-10-24 10:32:28 -0500

    Revised the susie_memory.R script.

--- inst/code/susie_memory.R
+++ inst/code/susie_memory.R
@@ -7,13 +7,13 @@ p <- 12000 # 8000
 n <- 4000 # 1000
 X <- matrix(rnorm(n*p),n,p)
 y <- rnorm(n)
-ss <- susieR:::univariate_regression(X,y)
-z <- ss$betahat/ss$sebetahat
+# ss <- susieR:::univariate_regression(X,y)
+# z <- ss$betahat/ss$sebetahat
 # R <- cor(X)
 cat("Size of X:\n")
 print(object.size(X),unit = "GB")
-cat("Size of R:\n")
-print(object.size(R),unit = "GB")
+# cat("Size of R:\n")
+# print(object.size(R),unit = "GB")
 cat("Running susie_rss.\n")
 out <- susie(X,y,estimate_prior_variance = FALSE,min_abs_corr = 0,
              verbose = TRUE)
