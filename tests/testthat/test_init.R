71d61f770999533b8ab75f1e11bf4dcff371ff8e
Gao Wang <wangow@gmail.com>
2022-04-19 13:24:38 -0400

    Fix a unit test

--- tests/testthat/test_init.R
+++ tests/testthat/test_init.R
@@ -3,6 +3,6 @@ context("test_init.R")
 test_that("susie init", with(simulate(sparse=T), {
   original.res  = readRDS('susiefit_original_res.rds')
   original.res$lbf_variable = matrix(0, nrow(original.res$alpha), ncol(original.res$alpha))
-  expect_warning(capture_output(susie(X, y, L=2, tol=1E-2, s_init = original.res, estimate_prior_variance = FALSE,verbose=T)))
+  expect_message(capture_output(susie(X, y, L=2, tol=1E-2, s_init = original.res, estimate_prior_variance = FALSE,verbose=T)))
   expect_message(capture_output(susie(X, y, L=20, tol=1E-2, s_init = original.res, estimate_prior_variance = FALSE, verbose=T)))
 }))
