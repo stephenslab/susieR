84c7191e5d6948a1606f800d3d6cd2d9c64fe526
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-08-22 09:34:17 -0500

    Added a couple tests for compute_colSds.

---
+++ tests/testthat/test_compute_colsds.R
@@ -0,0 +1,21 @@
+context("test_compute_colsds.R")
+
+test_that(paste("Check that compute_colSds gives same result as",
+                "matrixStats::colSds for a dense matrix"),{
+  set.seed(1)
+  X  <- matrix(rnorm(24),4,6)
+  y1 <- compute_colSds(X)
+  y2 <- matrixStats::colSds(X)
+  expect_equal(y1,y2,scale = 1,tolerance = 1e-15)
+})
+
+test_that(paste("Check that compute_colSds gives same result as",
+                "matrixStats::colSds for a sparse matrix"),{
+  set.seed(1)
+  X  <- matrix(rnorm(24),4,6)
+  X[runif(24) < 0.8] <- 0
+  Y  <- as(X,"CsparseMatrix")
+  y1 <- compute_colSds(Y)
+  y2 <- matrixStats::colSds(X)
+  expect_equal(y1,y2,scale = 1,tolerance = 1e-15)
+})
