579bc04393ab990c6569e6c54f03aeb3dc7c7ced
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-06-24 10:21:19 -0500

    Revised the susie_small tests.

--- tests/testthat/test_susie_small.R
+++ tests/testthat/test_susie_small.R
@@ -1,10 +1,16 @@
+context("test_susie_small.R")
 
-n=70
-p=100
-
-X= matrix(rnorm(n*p),ncol=p)
-y= X[,1]+ rnorm(n)
-test_that("check that susie small runs correctly",   {
-  res1 = susie(X, y, small = TRUE)
-  expect_equal(res1$sets$cs[[1]],1)
+test_that(paste("check that susie-small runs correctly in a data set",
+                "with 1 causal SNP"),{
+  set.seed(1)
+  n <- 70
+  p <- 100
+  X <- matrix(rnorm(n*p),nrow = n,ncol = p)
+  X <- cbind(X[,1],X)
+  y <-  X[,1] + rnorm(n)
+  res1 <- susie(X,y,L = 1,small = FALSE,max_iter = 10,min_abs_corr = 0,
+                verbose = TRUE)
+  res2 <- susie(X,y,L = 1,small = TRUE,max_iter = 10,min_abs_corr = 0,
+                verbose = TRUE)
+  expect_equal(res1$sets,res2$sets,scale = 1,tolerance = 1e-4)
 })
