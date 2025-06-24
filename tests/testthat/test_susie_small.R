f7dda31787e789e4363d6c497ee5e13c998b40f9
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-06-24 10:49:08 -0500

    Added a couple more tests for susie() with small = TRUE.

--- tests/testthat/test_susie_small.R
+++ tests/testthat/test_susie_small.R
@@ -8,9 +8,14 @@ test_that(paste("check that susie-small runs correctly in a data set",
   X <- matrix(rnorm(n*p),nrow = n,ncol = p)
   X <- cbind(X[,1],X)
   y <-  X[,1] + rnorm(n)
-  res1 <- susie(X,y,L = 1,small = FALSE,max_iter = 10,min_abs_corr = 0,
-                verbose = TRUE)
-  res2 <- susie(X,y,L = 1,small = TRUE,max_iter = 10,min_abs_corr = 0,
-                verbose = TRUE)
+  capture.output(res1 <- susie(X,y,L = 1,small = FALSE,max_iter = 10,
+                               min_abs_corr = 0,verbose = TRUE))
+  capture.output(res2 <- susie(X,y,L = 1,small = TRUE,max_iter = 10,
+                               min_abs_corr = 0,verbose = TRUE))
+  capture.output(res3 <- susie(X,y,small = FALSE,max_iter = 10,
+                               verbose = TRUE))
+  suppressWarnings(capture.output(
+    res4 <- susie(X,y,small = TRUE,max_iter = 10,verbose = TRUE)))
   expect_equal(res1$sets,res2$sets,scale = 1,tolerance = 1e-4)
+  expect_equal(res3$sets,res4$sets,scale = 1,tolerance = 1e-4)
 })
