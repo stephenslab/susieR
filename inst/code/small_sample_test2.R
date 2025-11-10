eb2e3d54ed5ea96a70ad1f2403fec669c90fbed5
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-11-10 10:30:48 -0600

    Added a small script small_sample_test2.R to verify estimation of V when V turns out to be quite large (>8).

---
+++ inst/code/small_sample_test2.R
@@ -0,0 +1,7 @@
+set.seed(1)
+load("../datafiles/small_sample_test2.RData")
+fit <- susie(X,y,L = 1,small = TRUE,alpha0 = 0.1,beta0 = 0.1,
+             verbose = TRUE)
+print(diff(fit$elbo))
+print(fit$V)
+
