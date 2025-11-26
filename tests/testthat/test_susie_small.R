6d66ffe683b73c990ffb2928a146c0998d6b82f5
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-11-26 08:04:11 -0600

    Fixed a test.

--- tests/testthat/test_susie_small.R
+++ tests/testthat/test_susie_small.R
@@ -7,7 +7,7 @@ test_that(paste("check that ELBO is monotonically increasing for ",
   data(data_small)
   y <- data_small$y
   X <- data_small$X
-  fit1 <- susie(X,y,L = 1,estimate_residual_method = "Servin_Stephens",
-                alpha0 = 0.1,beta0 = 0.1,tol = 1e-6,verbose = TRUE)
+  fit <- susie(X,y,L = 1,estimate_residual_method = "Servin_Stephens",
+               alpha0 = 0.1,beta0 = 0.1,tol = 1e-6,verbose = TRUE)
   expect_true(all(diff(fit$elbo) >= 0))
 })
