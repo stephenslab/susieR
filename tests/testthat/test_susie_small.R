5829dbd6bc82c415350136c6669838383b467ce2
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-11-26 08:03:22 -0600

    Added test for estimate_residual_method = 'Servin_Stephens'.

--- tests/testthat/test_susie_small.R
+++ tests/testthat/test_susie_small.R
@@ -1,22 +1,13 @@
 context("test_susie_small.R")
 
-test_that(paste("check that susie-small runs correctly in a data set",
-                "with 1 causal SNP"),{
-  skip()
+test_that(paste("check that ELBO is monotonically increasing for ",
+                "estimate_residual_method = 'Servin_Stephens', ",
+                "with L = 1"),{
   set.seed(1)
-  n <- 70
-  p <- 100
-  X <- matrix(rnorm(n*p),nrow = n,ncol = p)
-  X <- cbind(X[,1],X)
-  y <-  X[,1] + rnorm(n)
-  capture.output(res1 <- susie(X,y,L = 1,small = FALSE,max_iter = 10,
-                               min_abs_corr = 0,verbose = TRUE))
-  capture.output(res2 <- susie(X,y,L = 1,small = TRUE,max_iter = 10,
-                               min_abs_corr = 0,verbose = TRUE))
-  capture.output(res3 <- susie(X,y,small = FALSE,max_iter = 10,
-                               verbose = TRUE))
-  suppressWarnings(capture.output(
-    res4 <- susie(X,y,small = TRUE,max_iter = 10,verbose = TRUE)))
-  expect_equal(res1$sets,res2$sets,scale = 1,tolerance = 1e-4)
-  expect_equal(res3$sets,res4$sets,scale = 1,tolerance = 1e-4)
+  data(data_small)
+  y <- data_small$y
+  X <- data_small$X
+  fit1 <- susie(X,y,L = 1,estimate_residual_method = "Servin_Stephens",
+                alpha0 = 0.1,beta0 = 0.1,tol = 1e-6,verbose = TRUE)
+  expect_true(all(diff(fit$elbo) >= 0))
 })
