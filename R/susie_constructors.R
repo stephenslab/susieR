52f94a021e94fb5e0050dede45a6c33e36320afc
alexmccreight <57416850+alexmccreight@users.noreply.github.com>
2026-03-22 16:43:38 -0500

    warning message

--- R/susie_constructors.R
+++ R/susie_constructors.R
@@ -951,7 +951,8 @@ rss_lambda_constructor <- function(z, R = NULL, X = NULL, n = NULL,
       ))
     }
     if (!is_symmetric_matrix(R)) {
-      stop("R is not a symmetric matrix.")
+      warning_message("R not symmetric; using (R + t(R))/2.")
+      R <- (R + t(R)) / 2
     }
     if (!(is.double(R) & is.matrix(R)) & !inherits(R, "sparseMatrix")) {
       stop("Input R must be a double-precision matrix or a sparse matrix.")
